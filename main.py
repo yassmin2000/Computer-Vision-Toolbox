9d8f808a2a8c84b1d7b65afe8f7fa7b343e066b1
yassmin2000 <105241119+yassmin2000@users.noreply.github.com>
2024-03-01 22:26:45 +0200

    browse and display

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtWidgets,uic,QtGui
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import QGraphicsPixmapItem, QFileDialog, QGraphicsScene
+import numpy as np
 import sys
 import cv2
 class mainwindow (QtWidgets.QMainWindow):
@@ -7,15 +9,20 @@ class mainwindow (QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         uic.loadUi("mainwindow.ui",self)
         self.original_img =[]
+        self.gray_img=[]
         self.processed_img=[]
+        self.hybrid_input=[]
         self.filters_combo.currentIndexChanged.connect(self.handlecomboBoxChange)
         self.comboBox_2.currentIndexChanged.connect(self.handlecomboBoxChange2)
-        self.graphicsView.mouseDoubleClickEvent = lambda event: self.browse_and_display_image()
+        self.graphicsView.mouseDoubleClickEvent = lambda event: self.browse_image(self.graphicsView)
+        self.graphicsView_3.mouseDoubleClickEvent = lambda event: self.browse_image(self.graphicsView_3)
+        self.graphicsView_5.mouseDoubleClickEvent = lambda event: self.browse_image(self.graphicsView_5)
         self.label_3.setVisible(True)
         self.comboBox_2.setVisible(True)
         self.comboBox_2.clear()
         new_options = ["Uniform", "Gaussian", "Salt & pepper"]
         self.comboBox_2.addItems(new_options)
+        self.radioButton.setChecked(True)
 
     def handlecomboBoxChange(self):
         if self.filters_combo.currentIndex() ==0:
@@ -49,13 +56,32 @@ class mainwindow (QtWidgets.QMainWindow):
             self.comboBox_2.clear()
             new_options = ["Global", "Local"]
             self.comboBox_2.addItems(new_options)
-            
-        elif self.filters_combo.currentIndex() ==4:
-             self.label_3.setVisible(False)
-             self.comboBox_2.setVisible(False)
+        
+        elif self.filters_combo.currentIndex()==6 or self.filters_combo.currentIndex()==7:
+            self.label_3.setVisible(False)
+            self.comboBox_2.setVisible(False)
+            self.label_13.setVisible(False)
+            self.label_21.setVisible(False)
+            self.label_22.setVisible(False)
+            self.label_23.setVisible(False)
+            self.horizontalSlider.setVisible(False)
+            self.horizontalSlider_2.setVisible(False)
+            self.horizontalSlider_3.setVisible(False)
+            self.horizontalSlider_4.setVisible(False)
+            self.label_4.setVisible(True)
+            self.label_4.setText("Cutoff frequency")
+            self.lineEdit.setVisible(True)
         else:
             self.label_3.setVisible(False)
             self.comboBox_2.setVisible(False)
+            self.label_13.setVisible(False)
+            self.label_21.setVisible(False)
+            self.label_22.setVisible(False)
+            self.label_23.setVisible(False)
+            self.horizontalSlider.setVisible(False)
+            self.horizontalSlider_2.setVisible(False)
+            self.horizontalSlider_3.setVisible(False)
+            self.horizontalSlider_4.setVisible(False)
             
     def handlecomboBoxChange2(self):
         if self.filters_combo.currentIndex() ==0:
@@ -155,6 +181,7 @@ class mainwindow (QtWidgets.QMainWindow):
                     self.comboBox.clear()
                     new_options = ["3x3", "5x5"]
                     self.comboBox.addItems(new_options)
+        
 
         elif self.filters_combo.currentIndex() ==3:
             if self.comboBox_2.currentIndex()==0: 
@@ -185,8 +212,8 @@ class mainwindow (QtWidgets.QMainWindow):
                 self.horizontalSlider_4.setVisible(True)
                 self.comboBox.setVisible(False)
                 self.label_12.setVisible(False)
-
-    def browse_and_display_image(self):
+        
+    def browse_image(self,widget):
             file_dialog = QFileDialog()
             file_dialog.setNameFilter("Images (*.png *.jpg *.bmp *.gif)")
             file_dialog.setFileMode(QFileDialog.ExistingFile)
@@ -195,10 +222,68 @@ class mainwindow (QtWidgets.QMainWindow):
                 selected_file = file_dialog.selectedFiles()[0]
                 # Create an instance of the Image class with the selected image path
                 self.original_img=  cv2.imread(selected_file)
-                # Resize the image to a specific width and height
-                target_width = 300  
-                target_height = 200  
-                self.original_img.resize_image(target_width, target_height)
+                self.hybrid_input=  cv2.imread(selected_file)
+                if widget == self.graphicsView: 
+                    if self.radioButton_2.isChecked():
+                        self.radioButton.setChecked(False)
+                        self.gray_img =self.convert_gry(self.original_img)
+                        # self.display_image(self.gray_img,widget)
+                        # self.display_image(self.gray_img,self.graphicsView_6)
+                    else:
+                        self.radioButton_2.setChecked(False)
+                        self.display_image(self.original_img,widget)
+                        # self.display_image(self.gray_img,self.graphicsView_6)
+                else:
+                     self.display_image(self.hybrid_input,widget)
+
+
+    def display_image(self, img_data, widget):
+        # Get the size of the widget
+        widget_width = widget.width()
+        widget_height = widget.height()
+        if self.radioButton.isChecked()==True:
+             img_height, img_width, _ = img_data.shape
+        # else:
+        #     img_height, img_width, _ = img_data.shape + (1,)
+        aspect_ratio = img_width / img_height
+
+        # Calculate the new width and height to maintain the aspect ratio
+        if aspect_ratio > 1:
+            new_width = widget_width
+            new_height = int(widget_width / aspect_ratio)
+        else:
+            new_height = widget_height
+            new_width = int(widget_height * aspect_ratio)
+
+        # Resize the image using OpenCV
+        resized_image = cv2.resize(img_data, (new_width, new_height))
+
+        # Convert the OpenCV image to QImage
+        height, width, _ = resized_image.shape
+        # + (1,)
+        bytes_per_line = 3 * width
+        q_image = QImage(resized_image.data, width, height, bytes_per_line, QImage.Format_RGB888)
+
+        # Create a QGraphicsScene
+        scene = QGraphicsScene(self)
+        widget.setScene(scene)
+
+        # Create a QPixmap from the QImage
+        pixmap = QPixmap.fromImage(q_image)
+
+        # Create a QGraphicsPixmapItem and add it to the scene
+        pixmap_item = QGraphicsPixmapItem(pixmap)
+        scene.addItem(pixmap_item)
+
+    def convert_gry(self, img_data):
+        if len(img_data.shape) == 3:  # Check if it's a color image
+            R, G, B = img_data[:,:,0], img_data[:,:,1], img_data[:,:,2]
+            update = R * 0.299 + G * 0.587 + B * 0.114
+            # Ensure that the values are normalized to the range [0, 255]
+            update = np.clip(update, 0, 255).astype(np.uint8)
+            return np.array(update)
+
+         
 
     def resize_image(self, target_width, target_height):
         # Resize the image to the specified width and height
